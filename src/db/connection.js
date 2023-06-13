22e6049d29874606ea15769353aea97ac66dc543
AwangMedidat <awangmedidat98@gmail.com>
2023-06-13 12:57:25 +0700

    Add Connection

---
+++ src/db/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+module.exports.connection = mongoose
+  .connect("mongodb://localhost:27017/disleksia", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((e) => {
+    console.log("no connection");
+  });
